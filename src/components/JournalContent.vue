e359aca672f6e58976e5d99c8ff9c809a4d712e3
白 <934752907@qq.com>
2021-02-02 16:29:13 +0800

    初始化

---
+++ src/components/JournalContent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="journal-content" v-html="markdownHtml(content)" />
+</template>
+
+<script>
+import MarkdonwIt from "markdown-it";
+const md = new MarkdonwIt();
+export default {
+  props: {
+    content: String,
+  },
+  methods: {
+    markdownHtml(markdown) {
+      return md.render(markdown);
+    },
+  },
+};
+</script>
+
+<style>
+p {
+  line-height: 1.5;
+  font-size: 1.15rem;
+}
+h2 {
+  font-size: 2rem;
+}
+h3 {
+  font-size: 1.5rem;
+}
+h4,
+h5,
+h6 {
+  font-size: 1.15rem;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,56 @@
+<template>
+	<Layout>
+		<div id="container">
+			<Hero />
+			<ProjectsGrid :projects="$page.projects.edges" />
+		</div>
+		<LatestJournals :journals="$page.journals.edges" />
+	</Layout>
+</template>
+
+<page-query>
+query {
+  projects: allStrapiProject {
+    edges {
+      node {
+        id
+        date (format: "YYYY")
+        title
+        categories {
+          title
+        }
+        thumbnail {
+          url (quality: 90)
+        }
+      }
+    }
+  }
+	journals: allStrapiJournal {
+    edges {
+      node {
+        id
+        title
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import Hero from "@/components/Hero"
+import ProjectsGrid from "@/components/ProjectsGrid"
+import LatestJournals from "@/components/LatestJournals"
+export default {
+	components: {
+		Hero,
+		ProjectsGrid,
+		LatestJournals
+	}
+};
+</script>
+
+<style>
+.home-links a {
+	margin-right: 1rem;
+}
+</style>
